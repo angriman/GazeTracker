1891889db1fe8e63c0791e6a404e182978ae3337
eug93 <eugenio.93@hotmail.it>
2016-05-10 15:00:36 +0200

    C++ opencv (maybe) working

--- app/src/main/java/com/teaminfernale/gazetracker/MainActivity.java
+++ app/src/main/java/com/teaminfernale/gazetracker/MainActivity.java
@@ -1,14 +1,20 @@
 package com.teaminfernale.gazetracker;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 
 public class MainActivity extends Activity {
@@ -39,14 +45,36 @@ public class MainActivity extends Activity {
         OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, mLoaderCallback);
     }
 
+    private Bitmap lena = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        lena = BitmapFactory.decodeResource(getResources(), R.drawable.lena1);
+        imageView.setImageBitmap(lena);
+
+        gaussianBlur();
+
         ((TextView) findViewById(R.id.textView)).setText("" + getMessage());
 
+    }
+
+    private Mat targetImage = null;
+
+    private void gaussianBlur() {
+        targetImage = new Mat();
+        Utils.bitmapToMat(lena, targetImage);
+        Imgproc.cvtColor(targetImage, targetImage, Imgproc.COLOR_BGR2RGB);
+
+        detectAndDisplay(targetImage.getNativeObjAddr());
+
+        Bitmap bitmap = Bitmap.createBitmap(targetImage.cols(), targetImage.rows(), Bitmap.Config.RGB_565);
+        Imgproc.cvtColor(targetImage, targetImage, Imgproc.COLOR_RGB2BGR);
+        Utils.matToBitmap(targetImage, bitmap);
+        ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
     }
 
 
@@ -55,6 +83,7 @@ public class MainActivity extends Activity {
     }
 
     public native int getMessage();
-    public native Mat detectAndDisplay(long matAddr);
+    public native void detectAndDisplay(long matAddr);
+
     //public native void gaussianBlur(long matAddr);
 }
--- app/src/main/jni/main-jni.cpp
+++ app/src/main/jni/main-jni.cpp
@@ -2,14 +2,19 @@
 #include <jni.h>
 #include "include/opencv2/calib3d.hpp"
 #include <opencv2/core/mat.hpp>
-
-JNIEXPORT jobject JNICALL
-Java_com_teaminfernale_gazetracker_MainActivity_detectAndDisplay(JNIEnv *env, jobject instance,
-                                                                 jlong matAddr) {
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/imgproc.hpp>
 
 
-    cv::Mat& frame = *(cv::Mat*)matAddr;
+JNIEXPORT void JNICALL
+Java_com_teaminfernale_gazetracker_MainActivity_detectAndDisplay(JNIEnv
+*env,
+jobject instance,
+        jlong
+matAddr) {
 
+cv::Mat& frame = *(cv::Mat*)matAddr;
+cv::cvtColor( frame, frame, CV_BGR2GRAY );
 }
 
 extern "C" {
