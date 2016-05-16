e7a109a653a4256b47323e90b49d35650eba7f1a
eug93 <eugenio.93@hotmail.it>
2016-05-16 11:52:23 +0200

    Added camera

--- app/src/main/java/com/teaminfernale/gazetracker/MainActivity.java
+++ app/src/main/java/com/teaminfernale/gazetracker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.teaminfernale.gazetracker;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
@@ -11,16 +12,26 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     private static final String TAG = "MainActivity";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat mRgba;
+    private Mat mGray;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -28,6 +39,11 @@ public class MainActivity extends Activity {
             switch (status) {
                 case LoaderCallbackInterface.SUCCESS: {
                     Log.i(TAG, "OpenCV loaded successfully");
+
+                    // Initialize camera
+                    mOpenCvCameraView.setCameraIndex(1);
+                    mOpenCvCameraView.enableFpsMeter();
+                    mOpenCvCameraView.enableView();
 
                 }
                 break;
@@ -53,6 +69,21 @@ public class MainActivity extends Activity {
         }
     }
 
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mOpenCvCameraView.disableView();
+    }
+
+
     private Bitmap lena = null;
 
     @Override
@@ -60,37 +91,59 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        lena = BitmapFactory.decodeResource(getResources(), R.drawable.lena1);
-        imageView.setImageBitmap(lena);
+      //  mOpenCvCameraView.setCvCameraViewListener(this);
 
-        gaussianBlur();
 
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.fd_activity_surface_view);
         ((TextView) findViewById(R.id.textView)).setText("" + getMessage());
 
     }
 
-    private Mat targetImage = null;
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        ImageView imageView = (ImageView) findViewById(R.id.fd_activity_surface_view);
+        Utils.matToBitmap(mRgba, lena); //BitmapFactory.decodeResource(getResources(), R.drawable.lena1);
+        imageView.setImageBitmap(lena);
+
+     //   gaussianBlur();
+
+        return mRgba;
+    }
 
     private void gaussianBlur() {
-        targetImage = new Mat();
+        Mat targetImage = new Mat();
         Utils.bitmapToMat(lena, targetImage);
         Imgproc.cvtColor(targetImage, targetImage, Imgproc.COLOR_BGR2RGB);
-
 
         filterImage(targetImage.getNativeObjAddr());
 
         Bitmap bitmap = Bitmap.createBitmap(targetImage.cols(), targetImage.rows(), Bitmap.Config.RGB_565);
         Imgproc.cvtColor(targetImage, targetImage, Imgproc.COLOR_RGB2BGR);
         Utils.matToBitmap(targetImage, bitmap);
-        ((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
+        ((ImageView) findViewById(R.id.fd_activity_surface_view)).setImageBitmap(bitmap);
     }
 
 
     static {
-
         System.loadLibrary("main-jni");
-
+        System.loadLibrary("opencv_java3");
     }
 
     public native int getMessage();
